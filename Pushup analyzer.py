042c856a17e9f92221d547548711df14cd64af42
SHIVANSH SAXENA <f20230637@pilani.bits-pilani.ac.in>
2026-06-05 09:28:15 +0530

    Update Pushup analyzer.py

--- Pushup analyzer.py
+++ Pushup analyzer.py
@@ -11,3 +11,18 @@ class PushupAnalyzer:
         self.pushup_count = 0
         self.position = "up"
         self.set_target = 5
+ # Live scores for each body part
+        self.head_score = 0
+        self.arm_score = 0
+        self.body_score = 0
+        self.leg_score = 0
+        
+        # Data collection for results table
+        self.rep_data = []
+        self.current_rep_feedback = []
+        
+        # Feedback mode
+        self.analyzing = True
+        self.feedback_mode = False
+        
+        print("Model loaded successfully!")
