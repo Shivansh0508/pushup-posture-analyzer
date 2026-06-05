af1e020840475a88a9af858a5b8d82f273dd9c04
SHIVANSH SAXENA <f20230637@pilani.bits-pilani.ac.in>
2026-06-05 09:28:44 +0530

    Update Pushup analyzer.py

--- Pushup analyzer.py
+++ Pushup analyzer.py
@@ -26,3 +26,14 @@ class PushupAnalyzer:
         self.feedback_mode = False
         
         print("Model loaded successfully!")
+        
+def calculate_angle(self, a, b, c):
+        """Calculate angle between three points (a-b-c), b is vertex"""
+        a = np.array(a)
+        b = np.array(b)
+        c = np.array(c)
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians * 180.0 / np.pi)
+        if angle > 180.0:
+            angle = 360 - angle
+        return angle
